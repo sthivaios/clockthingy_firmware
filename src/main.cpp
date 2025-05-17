bb3dbf6ebe1a374785b0ba4748459e05c3d0777b
Stratos Thivaios <sthivaios@kioydiolabs.org>
2025-05-17 04:19:07 +0300

    rtc module working

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,44 @@
 #include <Arduino.h>
+#include <RTClib.h>
+
+RTC_DS3231 rtc;
+
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+
 void setup() {
-// write your initialization code here
+
+    Serial.begin(115200);
+
+    if (! rtc.begin()) {
+        Serial.println("Couldn't find RTC");
+        Serial.flush();
+        while (1) delay(10);
+    }
 }
 
 void loop() {
-// write your code here
+
+    // Get the current time from the RTC
+    DateTime now = rtc.now();
+
+    // Getting each time field in individual variables
+    // And adding a leading zero when needed;
+    String yearStr = String(now.year(), DEC);
+    String monthStr = (now.month() < 10 ? "0" : "") + String(now.month(), DEC);
+    String dayStr = (now.day() < 10 ? "0" : "") + String(now.day(), DEC);
+    String hourStr = (now.hour() < 10 ? "0" : "") + String(now.hour(), DEC);
+    String minuteStr = (now.minute() < 10 ? "0" : "") + String(now.minute(), DEC);
+    String secondStr = (now.second() < 10 ? "0" : "") + String(now.second(), DEC);
+    String dayOfWeek = daysOfTheWeek[now.dayOfTheWeek()];
+
+    // Complete time string
+    String formattedTime = dayOfWeek + ", " + yearStr + "-" + monthStr + "-" + dayStr + " " + hourStr + ":" + minuteStr + ":" + secondStr;
+
+    // Print the complete formatted time
+    Serial.println(formattedTime);
+
+    Serial.println();
+    delay(3000);
+
 }
